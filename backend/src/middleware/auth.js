4cf6e8715ef5c14968274931891746b0ef45a87a
cantasar <62062865+cantasar@users.noreply.github.com>
2025-07-25 16:43:44 +0300

    feat: enhance authentication  /auth/me

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,7 @@
+export async function verifyToken(request, reply) {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.code(401).send({ error: 'Unauthorized' });
+  }
+}
--- backend/src/modules/user/service/user.service.js
+++ backend/src/modules/user/service/user.service.js
@@ -5,23 +5,3 @@ export async function findUserByEmail(email) {
   const user = await db.get('SELECT * FROM users WHERE email = ?', [email]);
   return user;
 }
-
-export async function createUser({ username, email, password }) {
-  const db = await initDB();
-  if (!username || !email || !password) {
-	throw new Error('Username, email, and password are required to create a user');
-  }
-  // Insert the new user into the database
-  const existingMail = await db.get('SELECT * FROM users WHERE email = ?', [email]);
-  if (existingMail) {
-	throw new Error('User with this email already exists');
-  }
-
-  const existingUsername = await db.get('SELECT * FROM users WHERE username = ?', [username]);
-  if (existingUsername) {
-	throw new Error('Username already taken');
-  }
-
-  const result = await db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, password]);
-  return result.lastID; // Return the ID of the newly created user
-}
