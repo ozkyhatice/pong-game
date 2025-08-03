6a976f965ff5814656ec70f38737cd703d957280
Hatice Özkaya <ozkyyhatice@gmail.com>
2025-08-03 19:56:08 +0300

    feat: game route added

--- backend/src/websocket/handlers/message.router.js
+++ backend/src/websocket/handlers/message.router.js
@@ -16,8 +16,8 @@ export async function routeMessage(message, userId) {
     case 'game':
       // Game modülüne yönlendir - şimdilik atla
       console.log('Game message received - will be implemented later');
-      // const { processGameMessage } = await import('../../modules/game/controller/game.controller.js');
-      // await processGameMessage(message, userId);
+      const { handleGameMessage} = await import('../../modules/game/controller/game.controller.js');
+      await handleGameMessage(msgObj, userId);
       break;
     
     default:
